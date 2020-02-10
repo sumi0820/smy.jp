951ea46638cc37468f42f46091a98ed0e2af6399
Sumiya Ushiro <u.sumi0820@gmail.com>
2020-02-10 19:48:31 +0800

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,180 +1,142 @@
-// const config = require('./src/utils/siteConfig')
-// const path = require(`path`)
+const config = require('./src/utils/siteConfig')
+const path = require(`path`)
 
-// const { createFilePath } = require('gatsby-source-filesystem');
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
 
-// exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
-// 	const { createNodeField } = boundActionCreators;
+  const loadPosts = new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allContentfulPost(
+          sort: { fields: [publishDate], order: DESC }
+          limit: 10000
+        ) {
+          edges {
+            node {
+              slug
+              publishDate
+            }
+          }
+        }
+      }
+    `).then(result => {
+      const posts = result.data.allContentfulPost.edges
+      const postsPerFirstPage = config.postsPerHomePage
+      const postsPerPage = config.postsPerPage
+      const numPages = Math.ceil(
+        posts.slice(postsPerFirstPage).length / postsPerPage
+      )
 
-// 	if (node.internal.type === `MarkdownRemark`) {
-// 		const value = createFilePath({ node, getNode });
-// 		createNodeField({
-// 			name: `slug`,
-// 			node,
-// 			value,
-// 		});
-// 	}
-// };
+      // Create main home page
+      createPage({
+        path: `/`,
+        component: path.resolve(`./src/templates/index.js`),
+        context: {
+          limit: postsPerFirstPage,
+          skip: 0,
+          numPages: numPages + 1,
+          currentPage: 1,
+        },
+      })
 
-// exports.createPages = ({ graphql, actions }) => {
-//   const { createPage } = actions
+      // Create additional pagination on home page if needed
+      Array.from({ length: numPages }).forEach((_, i) => {
+        createPage({
+          path: `/${i + 2}/`,
+          component: path.resolve(`./src/templates/index.js`),
+          context: {
+            limit: postsPerPage,
+            skip: i * postsPerPage + postsPerFirstPage,
+            numPages: numPages + 1,
+            currentPage: i + 2,
+          },
+        })
+      })
 
-//   const loadPosts = new Promise((resolve, reject) => {
-//     graphql(`
-//       {
-//         allContentfulPost(
-//           sort: { fields: [publishDate], order: DESC }
-//           limit: 10000
-//         ) {
-//           edges {
-//             node {
-//               slug
-//               publishDate
-//             }
-//           }
-//         }
-//       }
-//     `).then(result => {
-//       const posts = result.data.allContentfulPost.edges
-//       const postsPerFirstPage = config.postsPerHomePage
-//       const postsPerPage = config.postsPerPage
-//       const numPages = Math.ceil(
-//         posts.slice(postsPerFirstPage).length / postsPerPage
-//       )
+      // Create each individual post
+      posts.forEach((edge, i) => {
+        const prev = i === 0 ? null : posts[i - 1].node
+        const next = i === posts.length - 1 ? null : posts[i + 1].node
+        createPage({
+          path: `${edge.node.slug}/`,
+          component: path.resolve(`./src/templates/post.js`),
+          context: {
+            slug: edge.node.slug,
+            prev,
+            next,
+          },
+        })
+      })
+      resolve()
+    })
+  })
 
-//       // Create main home page
-//       createPage({
-//         path: `/`,
-//         component: path.resolve(`./src/templates/index.js`),
-//         context: {
-//           limit: postsPerFirstPage,
-//           skip: 0,
-//           numPages: numPages + 1,
-//           currentPage: 1,
-//         },
-//       })
+  const loadTags = new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allContentfulTag {
+          edges {
+            node {
+              slug
+              post {
+                id
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+      const tags = result.data.allContentfulTag.edges
+      const postsPerPage = config.postsPerPage
 
-//       // Create additional pagination on home page if needed
-//       Array.from({ length: numPages }).forEach((_, i) => {
-//         createPage({
-//           path: `/${i + 2}/`,
-//           component: path.resolve(`./src/templates/index.js`),
-//           context: {
-//             limit: postsPerPage,
-//             skip: i * postsPerPage + postsPerFirstPage,
-//             numPages: numPages + 1,
-//             currentPage: i + 2,
-//           },
-//         })
-//       })
+      // Create tag pages with pagination if needed
+      tags.map(({ node }) => {
+        const totalPosts = node.post !== null ? node.post.length : 0
+        const numPages = Math.ceil(totalPosts / postsPerPage)
+        Array.from({ length: numPages }).forEach((_, i) => {
+          createPage({
+            path:
+              i === 0 ? `/tag/${node.slug}/` : `/tag/${node.slug}/${i + 1}/`,
+            component: path.resolve(`./src/templates/tag.js`),
+            context: {
+              slug: node.slug,
+              limit: postsPerPage,
+              skip: i * postsPerPage,
+              numPages: numPages,
+              currentPage: i + 1,
+            },
+          })
+        })
+      })
+      resolve()
+    })
+  })
 
-//       // Create each individual post
-//       posts.forEach((edge, i) => {
-//         const prev = i === 0 ? null : posts[i - 1].node
-//         const next = i === posts.length - 1 ? null : posts[i + 1].node
-//         createPage({
-//           path: `${edge.node.slug}/`,
-//           component: path.resolve(`./src/templates/post.js`),
-//           context: {
-//             slug: edge.node.slug,
-//             prev,
-//             next,
-//           },
-//         })
-//       })
-//       resolve()
-//     })
-//   })
+  const loadPages = new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allContentfulPage {
+          edges {
+            node {
+              slug
+            }
+          }
+        }
+      }
+    `).then(result => {
+      const pages = result.data.allContentfulPage.edges
+      pages.map(({ node }) => {
+        createPage({
+          path: `${node.slug}/`,
+          component: path.resolve(`./src/templates/page.js`),
+          context: {
+            slug: node.slug,
+          },
+        })
+      })
+      resolve()
+    })
+  })
 
-//   const loadTags = new Promise((resolve, reject) => {
-//     graphql(`
-//       {
-//         allContentfulTag {
-//           edges {
-//             node {
-//               slug
-//               post {
-//                 id
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `).then(result => {
-//       const tags = result.data.allContentfulTag.edges
-//       const postsPerPage = config.postsPerPage
-
-//       // Create tag pages with pagination if needed
-//       tags.map(({ node }) => {
-//         const totalPosts = node.post !== null ? node.post.length : 0
-//         const numPages = Math.ceil(totalPosts / postsPerPage)
-//         Array.from({ length: numPages }).forEach((_, i) => {
-//           createPage({
-//             path:
-//               i === 0 ? `/tag/${node.slug}/` : `/tag/${node.slug}/${i + 1}/`,
-//             component: path.resolve(`./src/templates/tag.js`),
-//             context: {
-//               slug: node.slug,
-//               limit: postsPerPage,
-//               skip: i * postsPerPage,
-//               numPages: numPages,
-//               currentPage: i + 1,
-//             },
-//           })
-//         })
-//       })
-//       resolve()
-//     })
-//   })
-
-// //   // Remove deleted entries & assets.
-// //   // TODO figure out if entries referencing now deleted entries/assets
-// //   // are "updated" so will get the now deleted reference removed.
-
-// //   // function deleteContentfulNode (node) {
-// //   //   const id = node.sys.id
-// //   //   const localizedIds = locales.map((locale) => normalize.makeId({ id, currentLocale: locale.code, defaultLocale }))
-// //   //   localizedIds.forEach(id => deleteNode(id, getNode(id)))
-// //   // }
-
-// //   // currentSyncData.deletedEntries
-// //   //   .map(e => e.sys.id)
-// //   //   .forEach(id => deleteNode(id, getNode(id)))
-// //   //   .forEach(deleteContentfulNode)
-// //   // currentSyncData.deletedAssets
-// //   //   .map(e => e.sys.id)
-// //   //   .forEach(id => deleteNode(id, getNode(id)))
-// //   //   .forEach(deleteContentfulNode)
-
-
-
-//   const loadPages = new Promise((resolve, reject) => {
-//     graphql(`
-//       {
-//         allContentfulPage {
-//           edges {
-//             node {
-//               slug
-//             }
-//           }
-//         }
-//       }
-//     `).then(result => {
-//       const pages = result.data.allContentfulPage.edges
-//       pages.map(({ node }) => {
-//         createPage({
-//           path: `${node.slug}/`,
-//           component: path.resolve(`./src/templates/page.js`),
-//           context: {
-//             slug: node.slug,
-//           },
-//         })
-//       })
-//       resolve()
-//     })
-//   })
-
-
-  
-//   return Promise.all([loadPosts, loadTags, loadPages])
-// }
+  return Promise.all([loadPosts, loadTags, loadPages])
+}
--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -4,11 +4,11 @@ module.exports = {
   publisher: 'Sumiya', // Organization name used for SEO schema
   siteDescription:
     'A starter template to build amazing static websites with Gatsby, Contentful and Netlify',
-  siteUrl: 'https://github.com/sumi0820/smy.jp.git', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  siteUrl: 'https://smy-jp.netlify.com/', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
   postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
   postsPerPage: 6, // Number of posts shown on paginated pages
   author: 'Sumiya', // Author for RSS author segment and SEO schema
-  authorUrl: 'https://gcn.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
+  authorUrl: 'https://smy-jp.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: '@sumiya0820', // Change for Twitter Cards
   shortTitle: 'smy.jp', // Used for App manifest e.g. Mobile Home Screen
   shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
